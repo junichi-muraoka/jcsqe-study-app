7410395f6e06d750bb27080ee9d3300e402fe1d0
junichi-muraoka <junichi-muraoka@users.noreply.github.com>
2026-03-30 15:45:15 +0000

    deploy: 2c1c93875ac7d9e645aad619206709ff2faa7a87

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -5,13 +5,14 @@
   const J = global.JCSQE = global.JCSQE || {};
 
   J.firebaseConfig = {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: ''
-  };
+  apiKey: "AIzaSyDHD14As3Z0_i69V-Qu6pTUaeaq3XSME7g",
+  authDomain: "jcsqe-study-app.firebaseapp.com",
+  projectId: "jcsqe-study-app",
+  storageBucket: "jcsqe-study-app.firebasestorage.app",
+  messagingSenderId: "450426213178",
+  appId: "1:450426213178:web:d6dfbdd543486739dfb603",
+  measurementId: "G-RTSG13C4CY"
+};
 
   J.isFirebaseConfigured = function isFirebaseConfigured() {
     const c = J.firebaseConfig;
